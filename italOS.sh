c041baa4928467ae79e3d85e9bc6dae876a01855
Italo Guedes <italoguedes@alu.ufc.br>
2021-12-27 16:22:01 -0300

    fixing erros

--- italOS.sh
+++ italOS.sh
@@ -4,7 +4,7 @@ myName=`whoami`
 # a little welcoming message
 echo "#####################################"
 echo "Hello $myName, and welcome to italOS!"
-echo "#####################################
+echo "#####################################"
 echo "This script will guide you through the installation of italOS!"
 
 # install all packages from pkglis.txt
@@ -15,21 +15,21 @@ systemctl enable NetworkManager.service
 systemctl enable lightdm
 
 # if there isn't a .config folder, creates it
-[ `fd -HIg .config $HOME` != "0" ] && \
+[ `fd -HIg .config $HOME` ] || \
     mkdir $HOME/.config
 
 # gets the path to the alacritty config file if there is already one
-path=`fd -HI alacritty.yml $HOME`
+path=`fd -HIg alacritty.yml $HOME`
 # if there isn't an alacritty config file already copies the one italOS to $HOME/.config/alacritty/
-if [ path != "0" ];then
+if [ path ];then
+    echo "An alacritty config file was found, do you still want the italOS one? (it won't overwrite yours) [Y/n]"
+    read path
+    [ path = "n" ] || [ path = "N" ] || \
+        cp config-files/alacritty.yml $HOME/.config/alacritty/italOS-alacritty.yml
+else
     # if there isn't an $HOME/.config/alacritty folder, creates it
-    [ `fd -HIg .config/alacritty $HOME` = "0" ] && \
+    [ `fd -HIg .config/alacritty $HOME` ] || \
         mkdir $HOME/.config/alacritty
     # copy italOS alacritty config file to $HOME/.config/alacritty/
     cp config-files/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-else
-    echo "An alacritty config file was found, do you still want the italOS one? (it won't overwrite yours) [Y/n]"
-    read path
-    [ path -eq "n" || path -eq "n" ] || \
-        cp config-files/alacritty.yml $HOME/.config/alacritty/italOS-alacritty.yml
 fi
