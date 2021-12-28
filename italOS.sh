fd45e4faaec7b6aa356a783ceb92f8796d2fa654
Italo Guedes <italoguedes@alu.ufc.br>
2021-12-28 12:33:54 -0300

    modularizing the configuration and adding awesome config files

--- italOS.sh
+++ italOS.sh
@@ -33,3 +33,27 @@ else
     # copy italOS alacritty config file to $HOME/.config/alacritty/
     cp config-files/italOS-alacritty.yml $HOME/.config/alacritty/alacritty.yml
 fi
+
+# creates a folder in .config if necessary and copies there without overwriting a config file from italOS
+# the first parameter must be the name of the folder created in .config
+# the second parameter must be the name of the file
+# the third parameter must be the extension without the dot (e.g: py)
+function setConfigFile () {
+    # if there isn't a config file already copies the one italOS to $HOME/.config/alacritty/
+    if [ `fd -HIg $2.$3 $HOME` ];then
+        echo "A config file was found, do you still want the italOS one? (it won't overwrite yours) [Y/n]"
+        read userInput
+        # if the user's input is other than n or N copies the config file without overwriting
+        [ $userInput = "n" ] || [ $userInput = "N" ] || \
+            cp config-files/italOS-$2.$3 $HOME/.config/$1/italOS-$2.$3
+    else
+        # if there isn't an $HOME/.config/$1 folder, creates it
+        [ `fd -HIg .config/$1 $HOME` ] || \
+            mkdir $HOME/.config/$1
+        # copy italOS config file to $HOME/.config/$1/
+        cp config-files/italOS-$2.$3 $HOME/.config/$1/$2.$3
+    fi
+}
+
+setConfigFile awesome rc lua
+setConfigFile awesome theme lua
